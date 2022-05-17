2d906988a89e70ba18552f5c57c1f7ac5606c164
abodige29 <95070685+abodige29@users.noreply.github.com>
2022-05-17 14:27:07 +0530

    Update onetime_update_idm_br_heir_ofc.sql

--- onetime_update_idm_br_heir_ofc.sql
+++ onetime_update_idm_br_heir_ofc.sql
@@ -1,9 +1,9 @@
 update edw_external_input_data.idm_br_hier_ofc 
-set ofc_id=q1.ofc_id_new
+set ofc_id=q1.ofc_id_new,row_process_dtm=current_timestamp(6)
 from(
 select voltageprotect(clean_string(voltageaccess(ofc_id,'sorparty')),'sorparty') as ofc_id_new,
 voltageprotect(clean_string(lpad(voltageaccess(ofc_id,'sorparty'),10,'0')),'sorparty') as ofc_id_old
-from EDW_STAGING.IDM_BR_HIER_OFC_SNAPSHOT
+from EDW_STAGING.IDM_BR_HIER_OFC_SNAPSHOT where length(ofc_id)<>10
 )q1
 where source_system_id in ('15','356')
 and  q1.ofc_id_old=ofc_id;
