52c4e620e85dfd81b0ae27ac8aea96df0980f02e
abodige29 <abodige29@massmutual.com>
2022-05-17 13:52:55 +0530

    version 0.1

---
+++ onetime_update_idm_br_heir_ofc.sql
@@ -0,0 +1,9 @@
+update edw_external_input_data.idm_br_hier_ofc 
+set ofc_id=q1.ofc_id_new
+from(
+select voltageprotect(clean_string(voltageaccess(ofc_id,'sorparty')),'sorparty') as ofc_id_new,
+voltageprotect(clean_string(lpad(voltageaccess(ofc_id,'sorparty'),10,'0')),'sorparty') as ofc_id_old
+from EDW_STAGING.IDM_BR_HIER_OFC_SNAPSHOT
+)q1
+where source_system_id in ('15','356')
+and  q1.ofc_id_old=ofc_id;
