61227385e5e95f76cd1985216fb53bfa2d24f621
abodige29 <abodige29@massmutual.com>
2022-05-18 13:23:49 +0530

    version 0.1

--- onetime_update_idm_br_heir_ofc.sql
+++ onetime_update_idm_br_heir_ofc.sql
@@ -3,7 +3,7 @@ set ofc_id=q1.ofc_id_new,row_process_dtm=current_timestamp(6)
 from(
 select voltageprotect(clean_string(voltageaccess(ofc_id,'sorparty')),'sorparty') as ofc_id_new,
 voltageprotect(clean_string(lpad(voltageaccess(ofc_id,'sorparty'),10,'0')),'sorparty') as ofc_id_old
-from EDW_STAGING.IDM_BR_HIER_OFC_SNAPSHOT where length(ofc_id)<>10
+from EDW_STAGING.IDM_BR_HIER_OFC_SNAPSHOT where length(clean_string(voltageaccess(ofc_id,'sorparty')))<>10
 )q1
 where source_system_id in ('15','356')
 and  q1.ofc_id_old=ofc_id;
