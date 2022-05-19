7ce2778ead47bb4ef4a6928ef535ab412f809e30
abodige29 <abodige29@massmutual.com>
2022-05-19 15:34:35 +0530

    version 0.2

--- onetime_update_dim_phone.sql
+++ onetime_update_dim_phone.sql
@@ -1,3 +1,6 @@
-INSERT INTO edw.dim_phone (dim_phone_natural_key_hash_uuid,phone_nr,extension_nr,country_cde,area_cde,dial_nr,primary_phone_ind,begin_dt,begin_dtm,row_process_dtm,audit_id,logical_delete_ind,check_sum,current_row_ind,end_dt,end_dtm,source_system_id,restricted_row_ind,row_sid,update_audit_id,exchange_nr,source_system_phone_id,source_delete_ind) 
-VALUES ('2c4f5c40-866f-54f4-4ef6-cf633348309f','',NULL,NULL,NULL,NULL,NULL,'0001-01-01','0001-01-01 00:00:00.000','2020-01-21 00:00:00.000',-1,false,'2c4f5c40-866f-54f4-4ef6-cf633348309f',true,'9999-12-31','9999-12-31 23:59:59.999','-1',false,219750001,0,NULL,NULL,false);
+
+
+
+INSERT INTO edw.dim_phone (dim_phone_natural_key_hash_uuid,phone_nr,extension_nr,country_cde,area_cde,dial_nr,primary_phone_ind,begin_dt,begin_dtm,row_process_dtm,audit_id,logical_delete_ind,check_sum,current_row_ind,end_dt,end_dtm,source_system_id,restricted_row_ind,update_audit_id,exchange_nr,source_system_phone_id,source_delete_ind) 
+VALUES ('2c4f5c40-866f-54f4-4ef6-cf633348309f','',NULL,NULL,NULL,NULL,NULL,'0001-01-01','0001-01-01 00:00:00.000',current_timestamp(6),-1,false,uuid_gen(false::Boolean)::uuid,true,'9999-12-31','9999-12-31 23:59:59.999','-1',false,0,NULL,NULL,false);
 
