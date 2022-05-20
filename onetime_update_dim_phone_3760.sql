333204e1aab5d1cc462d45f3b4f9be4b3d96d41e
abodige29 <abodige29@massmutual.com>
2022-05-20 18:29:17 +0530

    delta code changes and 3760 update

--- onetime_update_dim_phone_3760.sql
+++ onetime_update_dim_phone_3760.sql
@@ -1,12 +1,9 @@
+/*update dummy record in rel_contact*/
 
-/* insert a dummy record for NULL phone */
-
-
-INSERT INTO edw.dim_phone (dim_phone_natural_key_hash_uuid,phone_nr,extension_nr,country_cde,area_cde,dial_nr,
-primary_phone_ind,begin_dt,begin_dtm,row_process_dtm,audit_id,logical_delete_ind,check_sum,current_row_ind,
-end_dt,end_dtm,source_system_id,restricted_row_ind,update_audit_id,
-exchange_nr,source_system_phone_id,source_delete_ind) 
-VALUES ('2c4f5c40-866f-54f4-4ef6-cf633348309f','',NULL,NULL,NULL,NULL,NULL,'0001-01-01'::date,'0001-01-01'::timestamp(6),current_timestamp(6),-1,false,uuid_gen(false::Boolean)::uuid,true,'9999-12-31','9999-12-31'::timestamp(6),'-1',false,-1,NULL,NULL,false);
+update edw.rel_contact_preference
+set dim_phone_natural_key_hash_uuid=uuid_gen(NULL)::uuid,
+row_process_dtm=current_timestamp(6)
+where dim_phone_natural_key_hash_uuid='2c4f5c40-866f-54f4-4ef6-cf633348309f';
 
 commit;
 
