e376e66939bccf851f32096f840183f58c079901
abodige29 <abodige29@massmutual.com>
2022-06-03 15:08:52 +0530

    no change version

---
+++ party_cdavntg1life_party_master_of_masters_xref_initial.sql
@@ -0,0 +1,290 @@
+/*
+    FileName: party_cdavntg1life_party_master_of_masters_xref.sql
+    Author: MM69917
+    Subject Area : Party
+    Source: CDA VNTG1 LIFE
+    Create Date:2021-09-24
+       
+    ===============================================================================================================
+    Version/JIRA Story#     Created By     Last_Modified_Date   Description
+    ---------------------------------------------------------------------------------------------------------------
+    TERSUN-3525             Party-Tier2    09/02                Initial version      
+    ------------------------------------------------------------------------------------------------------------------
+*/
+
+
+TRUNCATE TABLE EDW_WORK.PARTY_CDAVNTG1LIFE_PARTY_MASTER_OF_MASTERS_XREF;
+
+/* insert master records into work from tersun source view */
+
+INSERT /*+direct*/ INTO EDW_WORK.PARTY_CDAVNTG1LIFE_PARTY_MASTER_OF_MASTERS_XREF
+(
+    DIM_PARTY_NATURAL_KEY_HASH_UUID  
+    ,DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID  
+    ,PARTY_ID 
+    ,PARTY_PRIOR_ID 
+    ,SOR_PARTY_ID
+    ,PARTY_ID_TYPE_CDE 
+    ,BEGIN_DT  
+    ,BEGIN_DTM  
+    ,ROW_PROCESS_DTM 
+    ,AUDIT_ID  
+    ,LOGICAL_DELETE_IND  
+    ,CHECK_SUM  
+    ,CURRENT_ROW_IND  
+    ,END_DT  
+    ,END_DTM  
+    ,SOURCE_SYSTEM_ID  
+    ,RESTRICTED_ROW_IND  
+    ,UPDATE_AUDIT_ID   
+    ,SOURCE_DELETE_IND 
+)
+SELECT
+     DIM_PARTY_NATURAL_KEY_HASH_UUID  
+    ,DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID  
+    ,VOLTAGEPROTECT(PARTY_ID,'sorparty')                                  AS PARTY_ID 
+    ,VOLTAGEPROTECT(PARTY_PRIOR_ID,'sorparty')                            AS PARTY_PRIOR_ID  
+    ,VOLTAGEPROTECT(SOR_PARTY_ID,'sorparty')                              AS SOR_PARTY_ID
+    ,PARTY_ID_TYPE_CDE
+    ,BEGIN_DT  
+    ,BEGIN_DTM  
+    ,ROW_PROCESS_DTM 
+    ,AUDIT_ID  
+    ,LOGICAL_DELETE_IND  
+    ,CHECK_SUM  
+    ,CURRENT_ROW_IND  
+    ,END_DT  
+    ,END_DTM  
+    ,SOURCE_SYSTEM_ID  
+    ,RESTRICTED_ROW_IND  
+    ,UPDATE_AUDIT_ID   
+    ,SOURCE_DELETE_IND  
+FROM
+(
+    SELECT 
+     CASE WHEN (BEN_FRST_NM, BEN_MDL_NM, BEN_LST_NM, CARR_ADMIN_SYS_CD, HLDG_KEY_PFX, HLDG_KEY, HLDG_KEY_SFX, BEN_ARGMT_TXT, BEN_ROW_CNTR_CD) IS NULL 
+    	THEN UUID_GEN(NULL)::UUID 
+    	ELSE UUID_GEN(BEN_FRST_NM, BEN_MDL_NM, BEN_LST_NM, CARR_ADMIN_SYS_CD, HLDG_KEY_PFX, HLDG_KEY, HLDG_KEY_SFX, BEN_ARGMT_TXT, BEN_ROW_CNTR_CD)::UUID 
+    	END AS DIM_PARTY_NATURAL_KEY_HASH_UUID
+    ,CASE WHEN (BEN_FRST_NM,BEN_MDL_NM,BEN_LST_NM,CARR_ADMIN_SYS_CD, HLDG_KEY_PFX, HLDG_KEY, HLDG_KEY_SFX,BEN_ARGMT_TXT,BEN_ROW_CNTR_CD) IS NULL 
+    	THEN UUID_GEN(NULL)::UUID 
+    	ELSE UUID_GEN(BEN_FRST_NM, BEN_MDL_NM, BEN_LST_NM, CARR_ADMIN_SYS_CD, HLDG_KEY_PFX, HLDG_KEY, HLDG_KEY_SFX, BEN_ARGMT_TXT, BEN_ROW_CNTR_CD)::UUID 
+    	END AS DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID
+    ,COALESCE(BEN_FRST_NM,'')||COALESCE(BEN_MDL_NM,'')||COALESCE(BEN_LST_NM,'')||
+     COALESCE(CARR_ADMIN_SYS_CD,'')||COALESCE(HLDG_KEY_PFX,'')||COALESCE(HLDG_KEY,'')||COALESCE(HLDG_KEY_SFX,'')||
+     COALESCE(SUBSTRING(BEN_ARGMT_TXT,1,100),'')||COALESCE(BEN_ROW_CNTR_CD,'') AS PARTY_ID
+    ,COALESCE(BEN_FRST_NM,'')||COALESCE(BEN_MDL_NM,'')||COALESCE(BEN_LST_NM,'')||
+     COALESCE(CARR_ADMIN_SYS_CD,'')||COALESCE(HLDG_KEY_PFX,'')||COALESCE(HLDG_KEY,'')||COALESCE(HLDG_KEY_SFX,'')||
+     COALESCE(SUBSTRING(BEN_ARGMT_TXT,1,100),'')||COALESCE(BEN_ROW_CNTR_CD,'') AS PARTY_PRIOR_ID  
+    ,SOR_PARTY_ID
+    ,PARTY_ID_TYPE_CDE 
+    ,BEGIN_DT  
+    ,BEGIN_DTM  
+    ,ROW_PROCESS_DTM 
+    ,AUDIT_ID  
+    ,LOGICAL_DELETE_IND  
+    ,CHECK_SUM  
+    ,CURRENT_ROW_IND  
+    ,END_DT  
+    ,END_DTM  
+    ,SOURCE_SYSTEM_ID  
+    ,RESTRICTED_ROW_IND  
+    ,UPDATE_AUDIT_ID   
+    ,SOURCE_DELETE_IND 
+    ,ROW_NUMBER() OVER(PARTITION BY BEN_FRST_NM, BEN_MDL_NM, BEN_LST_NM, 
+                                    CARR_ADMIN_SYS_CD, HLDG_KEY_PFX, HLDG_KEY, HLDG_KEY_SFX, 
+                                    BEN_ARGMT_TXT, BEN_ROW_CNTR_CD 
+                       ORDER BY BEN_DATA_TO_DT DESC) AS RNK
+	FROM 
+	(
+    SELECT 
+	CLEAN_STRING(VOLTAGEACCESS(BEN_FRST_NM,'name'))               AS BEN_FRST_NM, 
+	CLEAN_STRING(VOLTAGEACCESS(BEN_MDL_NM,'name'))                AS BEN_MDL_NM,
+	CLEAN_STRING(VOLTAGEACCESS(BEN_LST_NM,'name'))                AS BEN_LST_NM, 
+	LPAD(CLEAN_STRING(HLDG_KEY),20,'0')                           AS HLDG_KEY,
+	CLEAN_STRING(VOLTAGEACCESS(BEN_ARGMT_TXT,'freeform'))         AS BEN_ARGMT_TXT,
+    CLEAN_STRING(BEN_ROW_CNTR_CD)                                 AS BEN_ROW_CNTR_CD,
+    NULL                                                          AS SOR_PARTY_ID,
+    'Mstr_prty_id'                                                AS PARTY_ID_TYPE_CDE,
+    '0001-01-01'::DATE                                            AS BEGIN_DT,
+    '0001-01-01'::TIMESTAMP(6)                                    AS BEGIN_DTM,
+    CURRENT_TIMESTAMP                                             AS ROW_PROCESS_DTM,
+    :audit_id                                                     AS AUDIT_ID,
+    FALSE::BOOLEAN                                                AS LOGICAL_DELETE_IND,
+    UUID_GEN(FALSE::BOOLEAN)::UUID                                AS CHECK_SUM,
+    TRUE::BOOLEAN                                                 AS CURRENT_ROW_IND,
+    '12/31/9999'::DATE                                            AS END_DT,
+    '12/31/9999 00:00:00'::TIMESTAMP(6)                           AS END_DTM,
+    '241'                                                         AS SOURCE_SYSTEM_ID,
+    FALSE::BOOLEAN                                                AS RESTRICTED_ROW_IND,
+    :audit_id                                                     AS UPDATE_AUDIT_ID,
+    FALSE::BOOLEAN                                                AS SOURCE_DELETE_IND,
+    CLEAN_STRING(CARR_ADMIN_SYS_CD)                               AS CARR_ADMIN_SYS_CD,
+    UDF_ISNUM_LPAD(CLEAN_STRING(HLDG_KEY_PFX), 20, '0', TRUE)     AS HLDG_KEY_PFX,
+    UDF_ISNUM_LPAD(CLEAN_STRING(HLDG_KEY_SFX), 20, '0', TRUE)     AS HLDG_KEY_SFX,
+    BEN_DATA_TO_DT                                                AS BEN_DATA_TO_DT
+    FROM PROD_STND_VW_TERSUN.BEN_DATA_VW SRC
+    WHERE SRC.SRC_SYS_ID = '38'
+	)SOURCE_DATASET
+)FINAL_DATASET  WHERE RNK=1;
+
+COMMIT;
+
+/* insert beneficiary records into work from tersun source view */
+
+INSERT /*+direct*/ INTO EDW_WORK.PARTY_CDAVNTG1LIFE_PARTY_MASTER_OF_MASTERS_XREF
+(
+    DIM_PARTY_NATURAL_KEY_HASH_UUID  
+    ,DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID  
+    ,PARTY_ID 
+    ,PARTY_PRIOR_ID 
+    ,SOR_PARTY_ID
+    ,PARTY_ID_TYPE_CDE 
+    ,BEGIN_DT  
+    ,BEGIN_DTM  
+    ,ROW_PROCESS_DTM 
+    ,AUDIT_ID  
+    ,LOGICAL_DELETE_IND  
+    ,CHECK_SUM  
+    ,CURRENT_ROW_IND  
+    ,END_DT  
+    ,END_DTM  
+    ,SOURCE_SYSTEM_ID  
+    ,RESTRICTED_ROW_IND  
+    ,UPDATE_AUDIT_ID   
+    ,SOURCE_DELETE_IND 
+)
+SELECT
+    DIM_PARTY_NATURAL_KEY_HASH_UUID  
+    ,DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID  
+    ,VOLTAGEPROTECT(PARTY_ID,'sorparty')                                  AS PARTY_ID 
+    ,VOLTAGEPROTECT(PARTY_PRIOR_ID,'sorparty')                            AS PARTY_PRIOR_ID  
+    ,VOLTAGEPROTECT(SOR_PARTY_ID,'sorparty')                              AS SOR_PARTY_ID
+    ,PARTY_ID_TYPE_CDE 
+    ,BEGIN_DT  
+    ,BEGIN_DTM  
+    ,ROW_PROCESS_DTM 
+    ,AUDIT_ID  
+    ,LOGICAL_DELETE_IND  
+    ,CHECK_SUM  
+    ,CURRENT_ROW_IND  
+    ,END_DT  
+    ,END_DTM  
+    ,SOURCE_SYSTEM_ID  
+    ,RESTRICTED_ROW_IND  
+    ,UPDATE_AUDIT_ID   
+    ,SOURCE_DELETE_IND  
+FROM
+(
+    SELECT 
+    CASE WHEN (BEN_FRST_NM,BEN_MDL_NM,BEN_LST_NM,CARR_ADMIN_SYS_CD, HLDG_KEY_PFX, HLDG_KEY, HLDG_KEY_SFX,BEN_ARGMT_TXT,BEN_ROW_CNTR_CD) IS NULL 
+    	THEN UUID_GEN(NULL)::UUID 
+    	ELSE UUID_GEN(BEN_FRST_NM, BEN_MDL_NM, BEN_LST_NM, CARR_ADMIN_SYS_CD, HLDG_KEY_PFX, HLDG_KEY, HLDG_KEY_SFX, BEN_ARGMT_TXT, BEN_ROW_CNTR_CD)::UUID 
+    	END AS DIM_PARTY_NATURAL_KEY_HASH_UUID
+    ,UUID_GEN(NULL)::UUID AS DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID
+    ,COALESCE(BEN_FRST_NM,'')||COALESCE(BEN_MDL_NM,'')||COALESCE(BEN_LST_NM,'')||
+     COALESCE(CARR_ADMIN_SYS_CD,'')||COALESCE(HLDG_KEY_PFX,'')||COALESCE(HLDG_KEY,'')||COALESCE(HLDG_KEY_SFX,'')||
+     COALESCE(SUBSTRING(BEN_ARGMT_TXT,1,100),'')||COALESCE(BEN_ROW_CNTR_CD,'') AS PARTY_ID
+    ,PARTY_PRIOR_ID  
+    ,SOR_PARTY_ID
+    ,PARTY_ID_TYPE_CDE 
+    ,BEGIN_DT  
+    ,BEGIN_DTM  
+    ,ROW_PROCESS_DTM 
+    ,AUDIT_ID  
+    ,LOGICAL_DELETE_IND  
+    ,CHECK_SUM  
+    ,CURRENT_ROW_IND  
+    ,END_DT  
+    ,END_DTM  
+    ,SOURCE_SYSTEM_ID  
+    ,RESTRICTED_ROW_IND  
+    ,UPDATE_AUDIT_ID   
+    ,SOURCE_DELETE_IND 
+    ,ROW_NUMBER() OVER(PARTITION BY BEN_FRST_NM, BEN_MDL_NM, BEN_LST_NM, 
+                                    CARR_ADMIN_SYS_CD, HLDG_KEY_PFX, HLDG_KEY, HLDG_KEY_SFX,
+                                    BEN_ARGMT_TXT, BEN_ROW_CNTR_CD 
+                       ORDER BY BEN_DATA_TO_DT DESC) AS RNK 
+	FROM 
+	(
+    SELECT 
+	CLEAN_STRING(VOLTAGEACCESS(BEN_FRST_NM,'name'))               AS BEN_FRST_NM, 
+	CLEAN_STRING(VOLTAGEACCESS(BEN_MDL_NM,'name'))                AS BEN_MDL_NM,
+	CLEAN_STRING(VOLTAGEACCESS(BEN_LST_NM,'name'))                AS BEN_LST_NM, 
+	LPAD(CLEAN_STRING(HLDG_KEY),20,'0')                           AS HLDG_KEY,
+	CLEAN_STRING(VOLTAGEACCESS(BEN_ARGMT_TXT,'freeform'))         AS BEN_ARGMT_TXT,
+    CLEAN_STRING(BEN_ROW_CNTR_CD)                                 AS BEN_ROW_CNTR_CD,
+    NULL                                                          AS PARTY_PRIOR_ID,
+    NULL                                                          AS SOR_PARTY_ID,
+    'Beneficiary'                                                 AS PARTY_ID_TYPE_CDE,
+    '0001-01-01'::DATE                                            AS BEGIN_DT,
+    '0001-01-01'::TIMESTAMP(6)                                    AS BEGIN_DTM,
+    CURRENT_TIMESTAMP                                             AS ROW_PROCESS_DTM,
+    :audit_id                                                     AS AUDIT_ID,
+    FALSE::BOOLEAN                                                AS LOGICAL_DELETE_IND,
+    UUID_GEN(FALSE)::UUID                                         AS CHECK_SUM,
+    TRUE::BOOLEAN                                                 AS CURRENT_ROW_IND,
+    '12/31/9999'::DATE                                            AS END_DT,
+    '12/31/9999 00:00:00'::TIMESTAMP(6)                           AS END_DTM,
+    '241'                                                         AS SOURCE_SYSTEM_ID,
+    FALSE::BOOLEAN                                                AS RESTRICTED_ROW_IND,
+    :audit_id                                                     AS UPDATE_AUDIT_ID,
+    FALSE::BOOLEAN                                                AS SOURCE_DELETE_IND,
+    CLEAN_STRING(CARR_ADMIN_SYS_CD)                               AS CARR_ADMIN_SYS_CD,
+    UDF_ISNUM_LPAD(CLEAN_STRING(HLDG_KEY_PFX), 20, '0', TRUE)     AS HLDG_KEY_PFX,
+    UDF_ISNUM_LPAD(CLEAN_STRING(HLDG_KEY_SFX), 20, '0', TRUE)     AS HLDG_KEY_SFX,
+    BEN_DATA_TO_DT                                                AS BEN_DATA_TO_DT
+    FROM PROD_STND_VW_TERSUN.BEN_DATA_VW SRC
+    WHERE SRC.SRC_SYS_ID = '38'
+	)SOURCE_DATASET 
+)FINAL_DATASET WHERE RNK=1;
+
+/* delete VNTG1 records  from party_master */
+
+DELETE FROM EDW.PARTY_MASTER_OF_MASTERS_XREF WHERE SOURCE_SYSTEM_ID IN ('241','38');
+
+COMMIT;
+
+INSERT /*+direct*/ INTO EDW.PARTY_MASTER_OF_MASTERS_XREF
+(
+	 DIM_PARTY_NATURAL_KEY_HASH_UUID  
+	,DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID  
+	,PARTY_ID 
+	,PARTY_PRIOR_ID  
+	,PARTY_ID_TYPE_CDE 
+	,BEGIN_DT  
+	,BEGIN_DTM  
+	,ROW_PROCESS_DTM 
+	,AUDIT_ID  
+	,LOGICAL_DELETE_IND  
+	,CHECK_SUM  
+	,CURRENT_ROW_IND  
+	,END_DT  
+	,END_DTM  
+	,SOURCE_SYSTEM_ID  
+	,RESTRICTED_ROW_IND  
+	,UPDATE_AUDIT_ID   
+	,SOURCE_DELETE_IND 
+)
+SELECT
+     DIM_PARTY_NATURAL_KEY_HASH_UUID  
+    ,DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID  
+    ,PARTY_ID 
+    ,PARTY_PRIOR_ID  
+    ,PARTY_ID_TYPE_CDE 
+    ,BEGIN_DT  
+    ,BEGIN_DTM  
+    ,ROW_PROCESS_DTM 
+    ,AUDIT_ID  
+    ,LOGICAL_DELETE_IND  
+    ,CHECK_SUM  
+    ,CURRENT_ROW_IND  
+    ,END_DT  
+    ,END_DTM  
+    ,SOURCE_SYSTEM_ID  
+    ,RESTRICTED_ROW_IND  
+    ,UPDATE_AUDIT_ID   
+    ,SOURCE_DELETE_IND  
+FROM EDW_WORK.PARTY_CDAVNTG1LIFE_PARTY_MASTER_OF_MASTERS_XREF;
+
+COMMIT;
