171682f90ab49fe906a20eefaa486bafc720e9b8
abodige29 <abodige29@massmutual.com>
2022-07-18 16:49:31 +0530

    mdc_updt

--- onetime_update/mdc_update.sql
+++ onetime_update/mdc_update.sql
@@ -5,8 +5,3 @@ where lower(staging_table_name)='winrisk_edw_winrisk_requirements';
 commit;
 
 
-update edap_mdc.etl_project_staging_table
-set pk_columns='policy_number,case_id,case_reqt_id,requirement_status_code',updated_on=current_timestamp(6)
-where lower(staging_table_name)='tpp_edw_tpp_requirements';
-
-commit;
